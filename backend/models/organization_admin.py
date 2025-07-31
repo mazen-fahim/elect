ec8cc21ef35fab250cf8d4c997e76c8774006d85
Mazen Fahim <mazenfahim.g@gmail.com>
2025-07-31 13:45:11 +0300

    normalize candidate model

--- backend/models/organization_admin.py
+++ backend/models/organization_admin.py
@@ -5,15 +5,27 @@ from sqlalchemy import Integer, ForeignKey
 
 class OrganizationAdmin(Base):
     __tablename__ = "organization_admins"
+
+    # Foreign Keys
     user_id: Mapped[int] = mapped_column(
         Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
     )
-    user: Mapped["User"] = relationship("User", back_populates="organization_admin")
 
     organization_id: Mapped[int] = mapped_column(
         Integer, ForeignKey("organizations.user_id", ondelete="CASCADE"), nullable=False
     )
 
+    # Relationships
+    user: Mapped["User"] = relationship("User", back_populates="organization_admin")
+
     organization: Mapped["Organization"] = relationship(
         "Organization", back_populates="admins"
+    )
+
+    candidates: Mapped["Candidate"] = relationship(
+        "Candidate", back_populates="organization_admin"
+    )
+
+    elections: Mapped["Election"] = relationship(
+        "Election", back_populates="organization_admin"
     )
