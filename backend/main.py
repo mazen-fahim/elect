d26f2777edeca67a3569197999985bc5ca93243c
Rofaida Abdelnasser Abdelfatah <ra1775@fayoum.edu.eg>
2025-07-31 15:19:06 +0300

    endpoint to git all orgnizations

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from database import engine
 from fastapi import FastAPI
 from models import Base
+from routers import organization
+
 
 # This needs to be imported before we call create on Base.metadata
 # because it registers the models with SQLAlchemy
@@ -9,3 +11,4 @@ import models
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+app.include_router(organization.router)
