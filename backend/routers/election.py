7106ad2cb79819d136f1d9b89b67a9c9f7447c12
mazarona <mazenfahim.g@gmail.com>
2025-08-03 12:16:53 +0300

    feat: convert to async

--- backend/routers/election.py
+++ backend/routers/election.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from  models.election import Election
-from  database import get_db 
-from  schemas.election import ElectionCreate, ElectionUpdate, ElectionOut
+from models.election import Election
+from database import get_db
+from schemas.election import ElectionCreate, ElectionUpdate, ElectionOut
 
 router = APIRouter(prefix="/election", tags=["elections"])
+
 
 @router.get("/", response_model=list[ElectionOut])
 def get_all_elections(db: Session = Depends(get_db)):
     return db.query(Election).all()
+
 
 @router.get("/{election_id}", response_model=ElectionOut)
 def get_specific_election(election_id: int, db: Session = Depends(get_db)):
@@ -17,8 +19,11 @@ def get_specific_election(election_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Election not found")
     return election
 
+
 @router.put("/{election_id}", response_model=ElectionOut)
-def update_election(election_id: int, election_data: ElectionUpdate, db: Session = Depends(get_db)):
+def update_election(
+    election_id: int, election_data: ElectionUpdate, db: Session = Depends(get_db)
+):
     election = db.query(Election).filter(Election.id == election_id).first()
     if not election:
         raise HTTPException(status_code=404, detail="Election not found")
@@ -30,6 +35,7 @@ def update_election(election_id: int, election_data: ElectionUpdate, db: Session
     db.refresh(election)
     return election
 
+
 @router.delete("/{election_id}", status_code=204)
 def delete_election(election_id: int, db: Session = Depends(get_db)):
     election = db.query(Election).filter(Election.id == election_id).first()
@@ -38,4 +44,3 @@ def delete_election(election_id: int, db: Session = Depends(get_db)):
     db.delete(election)
     db.commit()
     return None
-
