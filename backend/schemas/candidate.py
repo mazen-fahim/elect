15eb7e0e32b81fffbb4d16c86da61c0e24c37847
Mohsen <mohamedmosen41@gmail.com>
2025-08-03 04:29:41 +0300

    add CandidateBase schema

---
+++ backend/schemas/candidate.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel , HttpUrl
+from typing import Optional
+from datetime import date
+
+
+class CandidateBase(BaseModel):
+    hashed_national_id : str 
+    name : str 
+    district : Optional[str] = None
+    governorate : Optional[str] = None
+    country : str 
+    party : str 
+    symbol_icon_url : Optional[HttpUrl] = None
+    photo_url : Optional[HttpUrl] = None 
+    birth_date : Optional[date] = None
+    description : Optional[str] = None
+    
+     
