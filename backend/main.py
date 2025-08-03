b05133455a9998598c313edb1a5e1f3cb2df3ac5
shereen <sherinshaban67@gmail.com>
2025-08-03 10:57:38 +0300

    feat(startup): implement async database initialization on application startup

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,15 @@
-from database import engine
+# main.py
 from fastapi import FastAPI
-from routers import organization,election
-
-# This needs to be imported before we call create on Base.metadata
-# because it registers the models with SQLAlchemy
+from database import engine
 import models
+from routers import organization, election
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(models.Base.metadata.create_all)
 
 app.include_router(organization.router)
 app.include_router(election.router)
